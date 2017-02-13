cec7207c995a1a395e5ba8478febb97e2e39c0bf
bram <brampijper@gmail.com>
2017-02-13 16:01:21 +0100

    Moved to new repo

---
+++ includes/main.js
@@ -0,0 +1,88 @@
+$ ("document").ready(function () {
+    $(".button-collapse").sideNav();
+        $('.scrollspy').scrollSpy();
+
+    //Changes the color of the header, slowly over time.     
+    var yellow = $("#header")
+    var speed = 8000;
+    function colorAnimate () {
+         $(yellow).animate({'background-color': '#1565C0'}, speed, function(){ 
+
+         $(yellow).animate({'background-color': '#607D8B'}, speed, function(){ 
+
+         $(yellow).animate({'background-color': '#FF5722'}, speed, function(){
+
+         colorAnimate();
+         }); 
+         }); 
+         });
+    }
+    colorAnimate();
+
+    $(document).on('scroll', function() {
+        if($(this).scrollTop()>=$('.skill-panel').innerHeight()){
+            jQuery('.skillbar').each(function(){
+                jQuery(this).find('.skillbar-bar').animate({
+                    width:jQuery(this).attr('data-percent')
+                },4500);
+            });
+        }
+    })
+}); 
+
+//Shows the animation in the about me section for extra information. 
+var $mainButton = $(".main-button"),
+    $closeButton = $(".close-button"),
+    $buttonWrapper = $(".button-wrapper"),
+    $layer = $(".layered-content");
+
+    $layer.hide()
+
+    $mainButton.on("click", function(){
+        $buttonWrapper.addClass("clicked")
+         setTimeout(function() {$layer.addClass("active"); }, 1500)
+         setTimeout(function() {$layer.show()}, 1500)
+    });
+
+    $closeButton.on("click", function(){
+        $buttonWrapper.removeClass("clicked");;
+        $layer.hide(); 
+    });
+
+//initializing google maps with a custom design
+function initMap() {
+
+    var styledMapType = new google.maps.StyledMapType(
+
+    [{"featureType":"administrative","elementType":"labels.text.fill","stylers":[{"color":"#444444"}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#f2f2f2"}]},{"featureType":"poi","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"all","stylers":[{"saturation":-100},{"lightness":45}]},{"featureType":"road.highway","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"road.arterial","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#46bcec"},{"visibility":"on"}]}],
+    {name: 'Styled Map'});
+    
+    var uluru = {lat: 52.3702, lng: 4.8952};
+    var map = new google.maps.Map(document.getElementById('map'), {
+        disableDefaultUI: true,
+        zoom: 13,
+        scrollwheel:  false,
+        center: uluru
+    });
+    var marker = new google.maps.Marker({
+        position: uluru,
+        map: map
+    });
+
+    google.maps.event.addDomListener(window, 'resize', function() {
+    map.setCenter((document.body.offsetWidth<1104)
+                    //<1104
+                  ? uluru
+                    //>=1104
+                  : uluru
+              );
+    });
+
+    google.maps.event.trigger(window, 'resize')
+
+    map.mapTypes.set('styled_map', styledMapType);
+    map.setMapTypeId('styled_map'); 
+}
+
+console.log('Hello! Reach out to me if you read this super secret message')
+
